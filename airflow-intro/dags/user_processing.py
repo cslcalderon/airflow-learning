ddb6bf88e300e79fd5d46f089283e82b95bbba3e
Sofia Calderon <cslcalderon@gmail.com>
2025-06-16 22:33:45 -0500

    completed first task sensor

--- airflow-intro/dags/user_processing.py
+++ airflow-intro/dags/user_processing.py
@@ -3,6 +3,7 @@ from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 
 # sensors check if something is true (if API is availible for example), true means sucess
 from airflow.sdk.bases.sensor import PokeReturnValue
+import requests
 
 
 @dag
@@ -20,6 +21,23 @@ def user_processing():
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)
             """,
     )
+
+    # checking condition, 300 is 5 minutes
+    @task.sensor(poke_interval=5, timeout=300)
+    def is_api_availible() -> PokeReturnValue:
+        response = requests.get(
+            "https://raw.githubusercontent.com/marclamberti/datasets/refs/heads/main/fakeuser.json"
+        )
+        print(response.status_code)
+        if response.status_code == 200:
+            condition = True
+            fake_user = response.json()
+        else:
+            condition = False
+            fake_user = None
+        return PokeReturnValue(is_done=condition, xcom_value=fake_user)
+
+    is_api_availible()
 
 
 # need to call or else you wont' see it on airflow UI

5895d400a1a18257269525add251b55425049f10
Sofia Calderon <cslcalderon@gmail.com>
2025-06-16 22:47:29 -0500

    inefficient python operator usage

--- airflow-intro/dags/user_processing.py
+++ airflow-intro/dags/user_processing.py
@@ -3,7 +3,28 @@ from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 
 # sensors check if something is true (if API is availible for example), true means sucess
 from airflow.sdk.bases.sensor import PokeReturnValue
-import requests
+
+# extract data with python
+from airflow.providers.standard.operators.python import PythonOperator
+
+
+def _extract_user(ti):
+    # takes in task instance to fetch data, ti is reserved keyword in airflow
+    # xcom is lightweight way to pass small vits of data from one task to another
+    # fake_user = ti.xcom_pull(task_ids="is_api_availible")  # from metadatabase
+
+    import requests
+
+    response = requests.get(
+        "https://raw.githubusercontent.com/marclamberti/datasets/refs/heads/main/fakeuser.json"
+    )
+    fake_user = response.json()
+    return {
+        "id": fake_user["id"],
+        "firstname": fake_user["personalInfo"]["firstName"],
+        "lastname": fake_user["personalInfo"]["lastName"],
+        "email": fake_user["personalInfo"]["email"],
+    }
 
 
 @dag
@@ -15,8 +36,8 @@ def user_processing():
         sql=""" 
         create table if not exists users (
             id INT PRIMARY KEY, 
-            firstname VARCHAR(255), 
-            lastname VARCHAR(255), 
+            firstName VARCHAR(255), 
+            lastName VARCHAR(255), 
             email VARCHAR(255), 
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)
             """,
@@ -25,6 +46,8 @@ def user_processing():
     # checking condition, 300 is 5 minutes
     @task.sensor(poke_interval=5, timeout=300)
     def is_api_availible() -> PokeReturnValue:
+        import requests
+
         response = requests.get(
             "https://raw.githubusercontent.com/marclamberti/datasets/refs/heads/main/fakeuser.json"
         )
@@ -36,6 +59,8 @@ def user_processing():
             condition = False
             fake_user = None
         return PokeReturnValue(is_done=condition, xcom_value=fake_user)
+
+    extract_user = PythonOperator(task_id="extract_user", python_callable=_extract_user)
 
     is_api_availible()
 

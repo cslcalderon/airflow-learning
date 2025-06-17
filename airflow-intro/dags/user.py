8aa616abbf5599edecd96890cb66d5bb3e5b4ae6
Sofia Calderon <cslcalderon@gmail.com>
2025-06-17 17:48:32 -0500

    making assets before optimizing creating multiple assets

--- airflow-intro/dags/user.py
+++ airflow-intro/dags/user.py
@@ -1,4 +1,6 @@
-from airflow.sdk import asset
+from airflow.sdk import asset, Asset, Context
+
+# context lets us grab the xcom
 
 
 @asset(schedule="@daily", uri="https://randomuser.me/api/")
@@ -10,3 +12,33 @@ def user(self) -> dict[str]:
 
 
 # don't need to call it explicitly like a dag
+
+
+# extracts location of the fake user that is passed through last asset
+@asset(schedule=user)  # will materialize when user materailizes
+def user_location(user: Asset, context: Context) -> dict[str]:
+    # fmt: off
+    user_data = context["ti"].xcom_pull(
+        dag_id=user.name, 
+        task_ids=user.name, 
+        include_prior_dates=True
+    )
+
+    return user_data['results'][0]['location']
+
+    # fmt: on
+
+
+# grabbing login information
+@asset(schedule=user)
+def user_login(user: Asset, context: Context) -> dict[str]:
+    # fmt: off
+    user_data = context['ti'].xcom_pull(
+        dag_id = user.name, 
+        task_ids = user.name, 
+        include_prior_dates=True
+    )
+   
+    return user_data['results'][0]['login']
+
+    # fmt: on

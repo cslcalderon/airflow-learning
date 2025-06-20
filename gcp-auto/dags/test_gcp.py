eb18a703ef6dc7ecb26781d09b52038c4ea42917
Sofia Calderon <cslcalderon@gmail.com>
2025-06-19 23:38:24 -0500

    updating env

--- gcp-auto/dags/test_gcp.py
+++ gcp-auto/dags/test_gcp.py
@@ -1,5 +1,5 @@
 # fmt: off
-from airflow import dag
+from airflow.sdk import dag, task
 from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
 from airflow.providers.google.cloud.operators.gcs import GCSCreateBucketOperator
 from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
@@ -7,3 +7,8 @@ from airflow.providers.google.cloud.transfers.gcs_to_local import GCSToLocalFile
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from airflow.sdk.bases.sensor import PokeReturnValue
 # fmt: on
+
+
+@dag
+def weather_data_grab():
+    pass
